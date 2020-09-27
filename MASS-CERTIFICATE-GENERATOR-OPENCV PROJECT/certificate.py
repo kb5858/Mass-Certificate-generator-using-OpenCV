a54e93ab668393b95bdd84c54d7b254b6c2d0667
kb5858 <62936271+kb5858@users.noreply.github.com>
2020-09-27 09:46:01 +0530

    Update certificate.py

--- MASS-CERTIFICATE-GENERATOR-OPENCV PROJECT/certificate.py
+++ MASS-CERTIFICATE-GENERATOR-OPENCV PROJECT/certificate.py
@@ -1,4 +1,8 @@
-#Generating the certificates
+
+##################################################
+######### GENERATING THE CERTIFICATES ############
+##################################################
+
 
 import cv2  
 import numpy as np  
@@ -6,6 +10,9 @@ import os
 import csv
 import xlrd   
 
+#######################################################
+######### ACCESSING THE CONTENT OF EXCEL SHEET#########
+#######################################################
 
 loc = ("data.xlsx")  
 
@@ -27,18 +34,24 @@ for i in range(sheet.nrows):
     img = cv2.imread("sample.jpg")  
 
     #select the font for the generated certificates 
-    font = cv2.FONT_HERSHEY_SIMPLEX
+    font1= cv2.FONT_HERSHEY_TRIPLEX
+    font3= cv2.FONT_HERSHEY_TRIPLEX
+    font2=cv2.FONT_HERSHEY_SCRIPT_COMPLEX
 
     # Draw the text 
 
-    img=cv2.putText(img, name, (int(coordinates[0]),int(coordinates[1])+15),font,1,(0,0,255),2)
-    img=cv2.putText(img, date, (int(coordinates[2]),int(coordinates[3])+15),font,1,(255,0,0),2)
-    img=cv2.putText(img, signature, (int(coordinates[4]),int(coordinates[5])+15),font,1,(0,255,0),2)
+    img=cv2.putText(img, name, (int(coordinates[0]),int(coordinates[1])+15),font1,1,(0,0,255),2)
+    img=cv2.putText(img, date, (int(coordinates[2]),int(coordinates[3])+15),font2,1,(255,0,0),2)
+    img=cv2.putText(img, signature, (int(coordinates[4]),int(coordinates[5])+15),font3,1,(0,255,0),2)
     
+    ###SHOW THE FIRST GENERATED CERTIFICATE
+
     if flag:
         cv2.imshow('Certificate', img) 
         flag=False
-        
+
+    ##Store the generated certificates in a folder
+       
     path = ''
     cv2.imwrite('./Generated_Certificates/'+name+'.png',img)
     
@@ -46,7 +59,3 @@ for i in range(sheet.nrows):
 
     cv2.destroyAllWindows()
     
-    
-
-
-
