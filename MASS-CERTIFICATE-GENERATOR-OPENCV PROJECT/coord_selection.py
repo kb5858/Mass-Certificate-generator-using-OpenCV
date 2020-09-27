b1bfdec15fb9c559a2921ecdf4b5a21689c51ad1
kb5858 <62936271+kb5858@users.noreply.github.com>
2020-09-27 09:44:13 +0530

    Update coord_selection.py

--- MASS-CERTIFICATE-GENERATOR-OPENCV PROJECT/coord_selection.py
+++ MASS-CERTIFICATE-GENERATOR-OPENCV PROJECT/coord_selection.py
@@ -1,5 +1,8 @@
 
-# Capturing mouse clicks
+#################################################
+######### Capturing Mouse Clicks#################
+#################################################
+
 
 import cv2 as cv
 import numpy as np
@@ -7,17 +10,22 @@ import numpy as np
 f = open("coordinates.txt","w")
 
 # mouse callback function
+#Function to select required coordinates and saving them in the folder 
 
 def draw_circle(event,x,y,flags,param):
     if event == cv.EVENT_LBUTTONDBLCLK:
         cv.putText(img,"Coordinates (%d,%d)"%(x,y),(80,60),3,1,(0,0,255)) 
         f.write(str(x)+"\n")                                           
-        f.write(str(y)+"\n")    
+        f.write(str(y)+"\n")   
 
+# Load image in OpenCV 
+#  
 img = cv.imread("sample.jpg")                                         
-                                                                          
+
+ #Creating a new window                                                                       
 cv.namedWindow('SELECTION')
 cv.setMouseCallback('SELECTION',draw_circle)
+
 while(1):
     cv.imshow('SELECTION',img)
     if cv.waitKey(10) & 0xFF == 27:   #Escape Key to terminate window
